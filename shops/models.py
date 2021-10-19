6bd230bfabd6dfe0559c0f252b5991cbb451c24f
Ivan Kitayeu <vanya.kitaev@gmail.com>
2021-10-19 16:34:55 +0300

    Added calculation of average rating

--- shops/models.py
+++ shops/models.py
@@ -38,6 +38,13 @@ class Product(models.Model):
         return reverse('shops:product_detail',
                        args=[self.category.slug, self.slug])
 
+    def get_average_review_score(self):
+        average_score = 0
+        if self.reviews.count() > 0:
+            total_score = sum([review.rating for review in self.reviews.all()])
+            average_score = total_score/self.reviews.count()
+        return round(average_score, 1)
+
 
 class Review(models.Model):
     product = models.ForeignKey(Product, related_name='reviews', on_delete=models.CASCADE)
