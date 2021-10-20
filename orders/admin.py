549ec97be8b0eff6ced6c74ba1d010391c5fe321
Ivan Kitayeu <vanya.kitaev@gmail.com>
2021-10-20 12:22:22 +0300

    Added models to admin app

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderItems
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItems
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'first_name', 'last_name', 'email',
+        'address', 'postal_code', 'city', 'paid',
+        'transport', 'created', 'status',
+    ]
+    list_filter = ['paid', 'created', 'updated']
+    inlines = [OrderItemInline]
