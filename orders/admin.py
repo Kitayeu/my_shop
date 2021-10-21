71ecca1a9552542780e8bdd8984f65ef6f0fe6ce
Ivan Kitayeu <vanya.kitaev@gmail.com>
2021-10-21 16:49:38 +0300

    Added export of orders to CSV in admin app

--- orders/admin.py
+++ orders/admin.py
@@ -1,4 +1,8 @@
+import csv
+import datetime
+
 from django.contrib import admin
+from django.http import HttpResponse
 
 from .models import Order, OrderItems
 from .tasks import status_change_notification
@@ -32,6 +36,27 @@ def status_completed(modeladmin, request, queryset):
     status_change(queryset, 'Completed')
 
 
+def export_csv(modeladmin, request, queryset):
+    chooses = modeladmin.model._meta
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = f'attachment; filename={chooses.verbose_name}.csv'
+    writer = csv.writer(response)
+    fields = [field for field in chooses.get_fields() if not field.many_to_many and not field.one_to_many]
+    writer.writerow([field.verbose_name for field in fields])
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+export_csv.short_description = 'Export to CSV'
+
+
 class OrderItemInline(admin.TabularInline):
     model = OrderItems
 
@@ -46,4 +71,6 @@ class OrderAdmin(admin.ModelAdmin):
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
 
-    actions = [status_created, status_processing, status_ready_for_pickup, status_shipped, status_completed]
+    actions = [status_created, status_processing, status_ready_for_pickup,
+               status_shipped, status_completed, export_csv
+               ]
