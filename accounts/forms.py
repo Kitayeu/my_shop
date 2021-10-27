1a47939779b0a3b71b7bf0bf3066068ace1cd11d
Ivan Kitayeu <vanya.kitaev@gmail.com>
2021-10-27 20:25:45 +0300

    Added CSS for 'register.html' and 'register_done.html'

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,23 +6,23 @@ from .models import Profile
 
 class UserRegistrationForm(forms.ModelForm):
     password2 = forms.CharField(
-        widget=forms.PasswordInput(attrs={'placeholder': 'Repeat password'}))
+        widget=forms.PasswordInput(attrs={'placeholder': 'Repeat password', 'class': 'form_user_registration'}))
 
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email', 'password')
         widgets = {
             'first_name': forms.TextInput(
-                attrs={'placeholder': 'First name'}
+                attrs={'placeholder': 'First name', 'class': 'form_user_registration'}
             ),
             'last_name': forms.TextInput(
-                attrs={'placeholder': 'Last name'},
+                attrs={'placeholder': 'Last name', 'class': 'form_user_registration'},
             ),
             'email': forms.EmailInput(
-                attrs={'placeholder': 'Email'}
+                attrs={'placeholder': 'Email', 'class': 'form_user_registration'}
             ),
             'password': forms.PasswordInput(
-                attrs={'placeholder': 'Password'}
+                attrs={'placeholder': 'Password', 'class': 'form_user_registration'}
             ),
         }
 
