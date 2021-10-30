b7e54ca2e92cf76c94c98e888021fa8be5f31709
Ivan Kitayeu <vanya.kitaev@gmail.com>
2021-10-30 22:46:57 +0300

    Changed database from sqlite3 to postgresql
    Updated requirements

--- my_shop/settings.py
+++ my_shop/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django.contrib.postgres',
 
     'shops',
     'carts',
@@ -98,8 +99,10 @@ WSGI_APPLICATION = 'my_shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'aqua',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
     }
 }
 
